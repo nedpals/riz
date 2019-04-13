17f42975979cd0927aafadfa25a4a1c5a7783365
Ned Palacios <npdoesmc@gmail.com>
2019-04-13 18:59:05 +0800

    add new site command

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ const cli = kuman.CLI();
 const buildCommand = require('./commands/build');
 const devCommand = require('./commands/dev');
 const getStateCommand = require('./commands/getState');
+const newSiteCommand = require('./commands/new');
 
 cli.command("build", buildCommand, {
     description: "Build pages from scratch"
@@ -19,6 +20,11 @@ cli.command("dev", devCommand, {
 
 cli.command("state", getStateCommand, {
     description: "Get the site configuration and it's current state"
+})
+
+cli.command("new", newSiteCommand, {
+    arguments: 1,
+    description: "Creates a new Riz website project."
 })
 
 module.exports = cli;
